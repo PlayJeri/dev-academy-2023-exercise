d0e1f4c83e2eead46049206a7e85ca8205e5a04f
PlayJeri <numri.jeri@gmail.com>
2023-02-04 08:05:07 +0200

    home and rides html created

--- bike_app/routes.py
+++ bike_app/routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for
 from .extensions import db
-from.models import Rides
-
+from .models import Rides
 
 
 views = Blueprint('views', __name__)
@@ -14,6 +13,6 @@ def home():
 
 @views.route('/rides/<int:page_num>')
 def rides(page_num):
-
+    rides = Rides.query.paginate(per_page=20, page=page_num, error_out=True)
 
     return render_template('index.html', rides=rides)
